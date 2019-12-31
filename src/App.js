e4411009ed769f9cc66bc49a248c63a6496ffe62
pinecat <rory.dudley@gmail.com>
2019-12-30 22:27:30 -0500

    adding the Timer component for the countdown

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import wallpaper from './resources/img/wallpaper.jpg';
 import './resources/css/finkheavy/finkheavyfont.css';
+import Timer from './components/Timer'
 
-function App() {
-  return (
-    <div style={ style } > 
-      <h1>Animal Crossing</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    var offset = new Date().getTimezoneOffset();
+    console.log(offset);
+  }
+
+  render() {
+    return (
+      <div style={ style } > 
+        <h1>Animal Crossing</h1>
+        <Timer />
+      </div>
+    );
+  }
 }
 
 const style = {
   width: '1920px',
   height: '1080px',
+  margin: 0,
   backgroundImage: `url(${ wallpaper })`,
   fontFamily: 'FinkHeavy',
   fontWeight: '900',
---
+++ src/components/Timer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Timer extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Timer;
